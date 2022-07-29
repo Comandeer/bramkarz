d481aa90d968e25132a9c8645244862b67bc39a8
Comandeer <vepomoc@gmail.com>
2022-07-29 20:20:21 +0200

    chore(preamble): minify preamble

---
+++ dev/preparePreamble.js
@@ -0,0 +1,20 @@
+/* eslint-disable no-console */
+
+import { readFile } from 'node:fs/promises';
+import { writeFile } from 'node:fs/promises';
+import { resolve as resolvePath } from 'node:path';
+import { cwd as processCwd } from 'node:process';
+import { minify } from 'terser';
+
+console.log( 'Preparing preamble…' );
+
+const cwd = processCwd();
+const fileName = 'bramkarzPreamble.js';
+const srcPath = resolvePath( cwd, 'src', fileName );
+const destPath = resolvePath( cwd, 'dist', fileName );
+const preamble = await readFile( srcPath, 'utf8' );
+const { code: minifiedPreamble } = await minify( preamble );
+
+await writeFile( destPath, minifiedPreamble );
+
+console.log( 'Preamble prepared.' );
