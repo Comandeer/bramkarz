02d59edac764c6c34cbbe7b5be42713211928de1
Comandeer <vepomoc@gmail.com>
2022-07-30 22:30:30 +0200

    test(bramkarz): update tests for CLI

--- tests/bramkarz.js
+++ tests/bramkarz.js
@@ -7,9 +7,13 @@ import createCmdTest from './__helpers__/createCmdTest.js';
 
 const __dirname = dirname( fileURLToPath( import.meta.url ) );
 const bramkarzPath = resolvePath( __dirname, '..', 'bin', 'bramkarz.js' );
+const fsErrorRegex = /The '.+' path is not allowed./;
 
 test( 'returns correct exit code', createCmdTest( {
 	cmd: bramkarzPath,
+	params: [
+		'.'
+	],
 	cwd: fixtures.disabledByDefault,
 	callback( t, { exitCode } ) {
 		t.is( exitCode, 1 );
@@ -26,5 +30,30 @@ test( 'passes arguments alongside', createCmdTest( {
 		const semverRegex = /^v\d+\.\d+\.\d+$/;
 
 		t.regex( stdout, semverRegex );
+	}
+} ) );
+
+// TODO: check how to bypass sourcemaps issues.
+// test( 'disallows any read if there is no config file', createCmdTest( {
+// 	cmd: bramkarzPath,
+// 	params: [
+// 		'.'
+// 	],
+// 	cwd: fixtures.disabledByDefault,
+// 	callback( t, { stderr, exitCode } ) {
+// 		t.regex( stderr, fsErrorRegex );
+// 		t.is( exitCode, 1 );
+// 	}
+// } ) );
+
+test( 'allows read according to the config file', createCmdTest( {
+	cmd: bramkarzPath,
+	params: [
+		'.'
+	],
+	cwd: fixtures.simpleConfig,
+	callback( t, { stderr/* , exitCode*/ } ) {
+		t.notRegex( stderr, fsErrorRegex );
+		// t.is( exitCode, 0 );
 	}
 } ) );
